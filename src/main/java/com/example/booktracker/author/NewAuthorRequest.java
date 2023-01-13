da336b3c7c8e0ab732302cc26aa5f6d005606166
Angelina <linaemalmros@gmail.com>
2023-01-13 15:44:21 +0100

    created dto classes for create/add requests

---
+++ src/main/java/com/example/booktracker/author/NewAuthorRequest.java
@@ -0,0 +1,11 @@
+package com.example.booktracker.author;
+
+import java.util.UUID;
+
+public class NewAuthorRequest {
+
+    UUID id;
+    String firstName;
+    String lastName;
+
+}
---
+++ src/main/java/com/example/booktracker/book/NewBookRequest.java
@@ -0,0 +1,15 @@
+package com.example.booktracker.book;
+
+import java.util.UUID;
+
+public class NewBookRequest {
+
+    UUID id;
+    String title;
+    String series;
+    String format;
+    Integer pages;
+    String publishDate;
+    Integer isbn;
+
+}
