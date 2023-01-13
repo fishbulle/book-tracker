23b5440dea6d411389ae069ce4117b7511c43dae
Angelina <linaemalmros@gmail.com>
2023-01-13 14:29:39 +0100

    started implementing author entity

--- src/main/java/com/example/booktracker/author/Author.java
+++ src/main/java/com/example/booktracker/author/Author.java
@@ -1,4 +1,35 @@
 package com.example.booktracker.author;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity(name = "authors")
+@Table(name = "Authors")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Author {
+
+    @Id
+    @Column(name = "author_id")
+    private UUID author_id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "first_name")
+    private String lastName;
+
+
 }
