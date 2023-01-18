e3de963a26482f94f8bb17a741a1c4fad53cae82
Angelina <linaemalmros@gmail.com>
2023-01-18 13:06:23 +0100

    created method to add new author

--- src/main/java/com/example/booktracker/author/NewAuthorRequest.java
+++ src/main/java/com/example/booktracker/author/NewAuthorRequest.java
@@ -2,10 +2,8 @@ package com.example.booktracker.author;
 
 import java.util.UUID;
 
-public class NewAuthorRequest {
-
-    UUID id;
-    String firstName;
-    String lastName;
-
+public record NewAuthorRequest(UUID id,
+                               String firstName,
+                               String lastName) {
 }
+
