0a593d5073d5e7bf7fc94e580c639a2be12bac2d
Angelina <linaemalmros@gmail.com>
2023-01-18 13:06:33 +0100

    created method to add new book

--- src/main/java/com/example/booktracker/book/NewBookRequest.java
+++ src/main/java/com/example/booktracker/book/NewBookRequest.java
@@ -1,15 +1,17 @@
 package com.example.booktracker.book;
 
+import com.example.booktracker.author.Author;
+
+import java.time.LocalDate;
 import java.util.UUID;
 
-public class NewBookRequest {
-
-    UUID id;
-    String title;
-    String series;
-    String format;
-    Integer pages;
-    String publishDate;
-    Integer isbn;
-
+public record NewBookRequest(UUID id,
+                             String title,
+                             Author author,
+                             String series,
+                             String format,
+                             Integer pages,
+                             LocalDate publishDate,
+                             String isbn) {
 }
+
